9eaae4c08512b9d51826d8ea1d0318f52e5f17a3
Yan Li <eyeplum@gmail.com>
2015-05-19 20:48:26 +0800

    Edit examples to make more sense.

--- demo/demo/ViewController.swift
+++ demo/demo/ViewController.swift
@@ -31,26 +31,34 @@ extension ViewController
   
   private var exampleFuriganas: [Furigana] {
     return [
+      // Furiganas for '田中さん、中華料理を食べたことありますか。'
       Furigana(text: "た", original: "田", range: NSMakeRange(0, 1)),
       Furigana(text: "なか", original: "中", range: NSMakeRange(1, 1)),
-      Furigana(text: "ちゅうかう", original: "中華", range: NSMakeRange(5, 2)), // The hiraganas are for demostration purpose only, they are actually incorrect
-      Furigana(text: "りょうり", original: "料理", range: NSMakeRange(7, 2)), // The hiraganas are for demostration purpose only, they are actually incorrect
+      Furigana(text: "ちゅう", original: "中", range: NSMakeRange(5, 1)),
+      Furigana(text: "か", original: "華", range: NSMakeRange(6, 1)),
+      Furigana(text: "りょう", original: "料", range: NSMakeRange(7, 1)),
+      Furigana(text: "り", original: "理", range: NSMakeRange(8, 1)),
       Furigana(text: "た", original: "食", range: NSMakeRange(10, 1)),
-      Furigana(text: "た", original: "田", range: NSMakeRange(22, 1)),
-      Furigana(text: "なか", original: "中", range: NSMakeRange(23, 1)),
-      Furigana(text: "ちゅうかう", original: "中華", range: NSMakeRange(27, 2)), // The hiraganas are for demostration purpose only, they are actually incorrect
-      Furigana(text: "りょうり", original: "料理", range: NSMakeRange(29, 2)), // The hiraganas are for demostration purpose only, they are actually incorrect
-      Furigana(text: "り", original: "理", range: NSMakeRange(30, 1)),
-      Furigana(text: "た", original: "食", range: NSMakeRange(32, 1)),
+      
+      // Furiganas for 'すみません。靴売り場はどこですか。'
+      Furigana(text: "くつ", original: "靴", range: NSMakeRange(29, 1)),
+      Furigana(text: "う", original: "売", range: NSMakeRange(30, 1)),
+      Furigana(text: "ば", original: "場", range: NSMakeRange(32, 1)),
     ]
   }
   
+  // Because we are using NSAttributedString as contents,
+  // most builtin attributes (e.g. NSFontAttributeName, NSForegroundColorAttributeName) will work just fine.
   private var exampleContents: NSAttributedString {
-    let contents = NSMutableAttributedString(string: "田中さん、中華料理を食べたことありますか。\n", attributes: [NSFontAttributeName : exampleFont])
+    let contents = NSMutableAttributedString(string: "田中さん、中華料理を食べたことありますか。\n\n", attributes: [NSFontAttributeName : exampleFont])
+    contents.addAttribute(NSForegroundColorAttributeName, value: UIColor.darkGrayColor(), range: NSMakeRange(0, 2))
     contents.addAttribute(NSForegroundColorAttributeName, value: UIColor.redColor(), range: NSMakeRange(5, 2))
+    contents.addAttribute(NSForegroundColorAttributeName, value: UIColor.brownColor(), range: NSMakeRange(7, 2))
+    contents.addAttribute(NSForegroundColorAttributeName, value: UIColor.greenColor(), range: NSMakeRange(10, 1))
     
-    contents.appendAttributedString(NSAttributedString(string: "田中さん、中華料理を食べたことありますか。", attributes: [NSFontAttributeName : exampleFontSansSerif]))
-    contents.addAttribute(NSForegroundColorAttributeName, value: UIColor.greenColor(), range: NSMakeRange(27, 2))
+    contents.appendAttributedString(NSAttributedString(string: "すみません。靴売り場はどこですか。", attributes: [NSFontAttributeName : exampleFontSansSerif]))
+    contents.addAttribute(NSForegroundColorAttributeName, value: UIColor.purpleColor(), range: NSMakeRange(29, 1))
+    contents.addAttribute(NSForegroundColorAttributeName, value: UIColor.orangeColor(), range: NSMakeRange(30, 3))
     
     return contents;
   }
